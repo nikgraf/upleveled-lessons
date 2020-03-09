77a05887acd225183af87e57b0d866a2dd42dcfe
Nik Graf <nik@nikgraf.com>
2020-03-09 12:38:31 +0100

    add perf issue example

--- lesson-react-hooks-and-good-practices/app/src/EffectApp/Title.js
+++ lesson-react-hooks-and-good-practices/app/src/EffectApp/Title.js
@@ -1,9 +1,19 @@
 import React, { useState, useEffect } from "react";
 
+// const myData = Array.from({ length: 10000000 }, () =>
+//   Math.floor(Math.random() * 40)
+// );
+
 function App() {
   const [count, setCount] = useState(0);
 
   useEffect(() => {
+    // const time0 = performance.now();
+    // const newData = myData.map(x => (x / 5) * 2325345);
+    // const newNewData = newData.map(x => (x * 2) / 99);
+    // const newNewNewData = newNewData.map(x => (x * 77) / 99);
+    // const time1 = performance.now();
+    // console.log("Took " + (time1 - time0) + " milliseconds.");
     document.title = `${count} new messages`;
   });
 
